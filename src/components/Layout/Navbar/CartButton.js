118828eaeee9650a9d79fc4395827369aed77558
Ryugi Suguro <rsuguro@gmail.com>
2021-06-30 23:22:52 +0900

    .reduce() added to update the total item numbers in cart on NavBar

--- src/components/Layout/Navbar/CartButton.js
+++ src/components/Layout/Navbar/CartButton.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import classes from "./CartButton.module.css";
+import CartContext from "../../../store/cart-context";
 
 const CartButton = (props) => {
+  const ctx = useContext(CartContext);
+
+  const { items } = ctx;
+
+  const numOfItems = items.reduce((currentNum, item) => {
+    return currentNum + item.amount;
+  }, 0);
+
   return (
     <button className={classes["cart-button"]} onClick={props.onClick}>
       {props.text}
-      <div className={classes["cart-counter"]}>0</div>
+      <div className={classes["cart-counter"]}>{numOfItems}</div>
     </button>
   );
 };
